9af8474563cd549ccceb78798e618497fbefedd6
merinomerino <32102477+merinomerino@users.noreply.github.com>
2017-09-30 21:44:00 -0500

    MiLinea

--- paint/src/Figura.java
+++ paint/src/Figura.java
@@ -3,12 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package paint;
 
 /**
  *
- * @author santo
+ * @author lokua
  */
-public class Panel {
+class Figura {
     
 }
